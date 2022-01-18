55df0a7ddad7c90bc86307dbff57f5643aa503ca
Mika Saha <kristian.saha@gmail.com>
2022-01-18 16:47:32 +0200

    compare dialog functions

--- comparedialog.cpp
+++ comparedialog.cpp
@@ -1,6 +1,8 @@
 #include "comparedialog.h"
 #include "ui_comparedialog.h"
 
+#include <QFileDialog>
+
 compareDialog::compareDialog(QWidget *parent) :
     QDialog(parent),
     dialogUi(new Ui::compareDialog)
@@ -11,6 +13,27 @@ compareDialog::compareDialog(QWidget *parent) :
 compareDialog::~compareDialog()
 {
     delete dialogUi;
+}
+
+QString compareDialog::SelectFile()
+{
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::AnyFile);
+    dialog.setNameFilter("All LOG files (*.LOG *.log)");
+    dialog.setViewMode(QFileDialog::Detail);
+    QStringList fileNames;
+
+    if (dialog.exec())
+    {
+        fileNames = dialog.selectedFiles();
+        return fileNames[0];
+    }
+    else
+        return "default";
+
+    QString file = SelectFile();
+    dialogUi->lineEdit->setText(file);
+
 }
 
 void compareDialog::on_compareButtonBox_accepted()
@@ -27,6 +50,25 @@ void compareDialog::on_compareButtonBox_rejected()
 
 void compareDialog::on_calendarWidget_activated(const QDate &date)
 {
+
+}
+
+
+void compareDialog::on_OpenFile1_clicked()
+{
+    QString file = SelectFile();
+    dialogUi->lineEdit->setText(file);
+    if (file != "default")
+        file1 = file;
+}
+
+
+void compareDialog::on_OpenFile2_clicked()
+{
+    QString file = SelectFile();
+    dialogUi->lineEdit_2->setText(file);
+    if (file != "default")
+        file2 = file;
 
 }
 
--- comparedialog.h
+++ comparedialog.h
@@ -2,6 +2,7 @@
 #define COMPAREDIALOG_H
 
 #include <QDialog>
+#include <QDate>
 
 namespace Ui {
 class compareDialog;
@@ -15,15 +16,24 @@ public:
     explicit compareDialog(QWidget *parent = nullptr);
     ~compareDialog();
 
+    QStringList GetFilenames();
+
 private slots:
     void on_compareButtonBox_accepted();
-
     void on_compareButtonBox_rejected();
-
     void on_calendarWidget_activated(const QDate &date);
+    void on_OpenFile1_clicked();
+    void on_OpenFile2_clicked();
+
+    QString SelectFile();
+
+
 
 private:
     Ui::compareDialog *dialogUi;
+
+    QString file1;
+    QString file2;
 };
 
 #endif // COMPAREDIALOG_H
